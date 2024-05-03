6c648b9aed2c5554d0eba1e3d682f86c53cd415a
Bharathi Chinnusamy <bharu@Bharathis-MacBook-Air.local>
2024-05-03 14:36:08 -0400

    added hashing for password

--- src/db/index.js
+++ src/db/index.js
@@ -15,3 +15,18 @@ app.listen(port, () => {
     console.log("server is up on port " + port)
 
 })
+
+const bcript = require('bcryptjs')
+
+// const myFunction = async () => {
+//     const password = 'Bharathi'
+//     const hasedPassword = await bcript.hash(password, 8)
+//     console.log(password)
+//     console.log(hasedPassword)
+
+//     const isMatch = await bcript.compare('Barathi', hasedPassword)
+//     console.log(isMatch)
+
+// }
+
+// myFunction()
